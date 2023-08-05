c65399a220d8b97e07c5511e897a2f57f3ded778
sayloure <135940846+sayloure@users.noreply.github.com>
2023-08-05 22:18:17 +0300

    fix

--- .folder/Scripts/activate.bat
+++ .folder/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\saylo\Desktop\учеба\gb\4. Знакомство с языком Python\seminars\1\.folder
+set VIRTUAL_ENV=C:\Users\saylo\Desktop\учеба\gb\4. Знакомство с языком Python\seminars\.folder
 
 if not defined PROMPT set PROMPT=$P$G
 
